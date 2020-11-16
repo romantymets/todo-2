ab69665e039cf7ea66ebe3e749f4b66c1b9a995b
romantymets <romantymets@gmail.com>
2020-11-16 20:44:44 +0200

    first commit

---
+++ src/TodoConteiner/TodoConteiner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import LoginContainer from "./containers/LoginContainer/LoginContainer";
+import './todoConteiner.css';
+
+
+class TodoConteiner extends React.Component {
+
+
+  render() {
+    return (
+      <h1> dvdfdfdfdsf </h1>
+    )
+  }
+}
+
+export default TodoConteiner;
