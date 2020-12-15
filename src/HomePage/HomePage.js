9eb495da9973f919710a598504eb0707d483b07c
romantymets <romantymets@gmail.com>
2020-12-15 20:38:43 +0200

    added navigations to site

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoConteiner from "../TodoConteiner/TodoConteiner";
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        <TodoConteiner />
+      </div>
+    );
+  }
+}
+
+export default HomePage;
--- src/Nawbar/Nawbar.js
+++ src/Nawbar/Nawbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import "./Nawbar.css";
 
 class Nawbar extends React.Component {
@@ -31,14 +31,14 @@ class Nawbar extends React.Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item ">
-                <a className="nav-link" href="#">
-                  Home <span className="sr-only">(current)</span>
-                </a>
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
+                <Link className="nav-link" to="/About">
                   About
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
